b8ffb0096468095d4faf2b1ec3c8c7b6b3f97ccc
lzkelley <lkelley@cfa.harvard.edu>
2013-12-27 15:53:56 -0800

    CosmoCalc.py : added inversions for look-back time (-tl) and universe age (-ta)

--- CosmoCalc.py
+++ CosmoCalc.py
@@ -32,13 +32,20 @@ class Settings(object):
     verbose    = True                                                                               # Print excess output to stdout
     print_flag = False                                                                              # Print the cosmological parameters
     build_flag = False                                                                              # Default whether to rebuild integration table
+
     use_pc     = False                                                                              # Use input distance in parsecs
     use_mpc    = False                                                                              # Use input distance in Megaparsecs
     use_ly     = False                                                                              # Use input distance in lightyears
 
-    z_target   = -1.0
-    cd_target  = -1.0
-    ld_target  = -1.0
+    use_yr     = False                                                                              # Use input time     in years
+    use_myr    = False                                                                              # Use input time     in megayears
+
+    # Target epochs (calculate epoch based on given parameter below)
+    z_target   = -1.0                                                                               # Redshift
+    cd_target  = -1.0                                                                               # Comoving Distance
+    ld_target  = -1.0                                                                               # Luminosity Distance
+    tl_target  = -1.0                                                                               # Lookback time
+    ta_target  = -1.0                                                                               # Age of the universe (time)
 
     root_tol   = 1.0e-10                                                                            # Root finding tolerance
     quad_iter  = 1000                                                                               # Number of iterations for quadrature
@@ -118,25 +125,14 @@ class Parameters(object):
 
 
 
-def Integrate(func, z0, z1):
-    val, err = sp.integrate.quadrature(func, z0, z1, maxiter=sets.quad_iter)
-    return val, err
-
-def IntegrateDistance(z0, z1):
-    dist, derr = Integrate(pars.HubbleDistanceFunction, z0, z1)
-    return dist, derr
-
-def IntegrateTime(z0, z1):
-    time, terr = Integrate(pars.HubbleTimeFunction, z0, z1)
-    return time, terr
 
 
 def DistToCM(tst, indist):
     """
     Based on unit settings, convert distance to centimeters.
 
-    Setup to accept parsecs (pc), megaparsecs (mpc), or lightyears (ly).  Stored
-    in Settings object (tst).
+    Setup to accept [default] centimeters (cm), parsecs (pc), megaparsecs (mpc),
+    or lightyears (ly).  Stored in Settings object (tst).
     """
     if(   tst.use_pc  ): indist *= pars.parsec
     elif( tst.use_mpc ): indist *= pars.parsec*(1.0e6)
@@ -144,13 +140,60 @@ def DistToCM(tst, indist):
     return indist
 
 
+def TimeToS(tst, intime):
+    """
+    Based on unit settings, convert time to seconds.
+
+    Setup to accept [default] seconds (s), years (yr), megayears (myr).  Stored
+    in Settings object (tst).
+    """
+    if(   tst.use_yr  ): intime *= pars.year
+    elif( tst.use_myr ): intime *= pars.year*(1.0e6)
+    return intime
+
+
+
+
+
+
+def Integrate(func, z0, z1):
+    """Integrate a general function (func) from 'z0' to 'z1'."""
+    val, err = sp.integrate.quadrature(func, z0, z1, maxiter=sets.quad_iter)
+    return val, err
+
+def IntegrateDistance(z0, z1):
+    """Integrate distance between given redshifts."""
+    dist, derr = Integrate(pars.HubbleDistanceFunction, z0, z1)
+    return dist, derr
+
+def IntegrateTime(z0, z1):
+    """Integrate time between given redshifts"""
+    time, terr = Integrate(pars.HubbleTimeFunction, z0, z1)
+    return time, terr
+
+
+
+def RootFind(func): return sp.optimize.newton( func, 1.0, tol=sets.root_tol )
+
 def InvertComDist(cdist):
+    """Given a comoving-distance, find redshift."""
     rhs = lambda x: IntegrateDistance(0.0, x)[0] - cdist
-    return sp.optimize.newton( rhs, 1.0, tol=sets.root_tol )
+    return RootFind(rhs)
 
 def InvertLumDist(ldist):
+    """Given a luminosity-distance, find redshift."""
     rhs = lambda x: (1.0+x)*IntegrateDistance(0.0, x)[0] - ldist
-    return sp.optimize.newton( rhs, 1.0, tol=sets.root_tol )
+    return RootFind(rhs)
+
+def InvertLookTime(ltime):
+    """Given a look-back time, find redshift."""
+    rhs = lambda x: IntegrateTime(0.0, x)[0] - ltime
+    return RootFind(rhs)
+
+def InvertAgeTime(atime):
+    """Given an age of the universe, find redshift."""
+    rhs = lambda x: 1.0 - IntegrateTime(0.0, x)[0] - atime
+    return RootFind(rhs)
 
 
     
@@ -193,7 +236,7 @@ def CosmologicalParameters(redz, pout=False):
     # Age
     ageTime       = pars.HubbleTime()*(1.0-time)
     ageTime_err   = lookTime_err
-    retstr       += "t    =  %+e +- %e  [s]   ~  %+e [Myr] : Age of the Universe\n" % (ageTime, ageTime_err, ageTime/(1.0e6*pars.year) )
+    retstr       += "T_A  =  %+e +- %e  [s]   ~  %+e [Myr] : Age of the Universe\n" % (ageTime, ageTime_err, ageTime/(1.0e6*pars.year) )
 
     # Distance Modulus
     distMod       = 5.0*np.log10(lumDist/(pars.parsec*10.0))
@@ -202,13 +245,13 @@ def CosmologicalParameters(redz, pout=False):
 
     if( pout ): print retstr
 
-    return [ [comDist, comDist_err, "Comoving Distance [cm]"] , \
-             [lumDist, lumDist_err, "Luminosity Distance [cm]"] , \
-             [angDist, angDist_err, "Angular Diameter Distance [cm]"] , \
-             [arcSize, arcSize_err, "Arcsecond Transverse Size [cm]"] , \
-             [lookTime, lookTime_err, "Lookback Time [s]"] , \
-             [ageTime, ageTime_err, "Age of the Universe [s]"] , \
-             [distMod, distMod_err, "Distance Modulus []"] ]
+    return [ [comDist , comDist_err , "Comoving Distance [cm]"        ] , \
+             [lumDist , lumDist_err , "Luminosity Distance [cm]"      ] , \
+             [angDist , angDist_err , "Angular Diameter Distance [cm]"] , \
+             [arcSize , arcSize_err , "Arcsecond Transverse Size [cm]"] , \
+             [lookTime, lookTime_err, "Lookback Time [s]"             ] , \
+             [ageTime , ageTime_err , "Age of the Universe [s]"       ] , \
+             [distMod , distMod_err , "Distance Modulus []"           ] ]
 
 
 
@@ -223,7 +266,7 @@ def RedshiftFromTarget(t_sets):
     parameter from a line from the table (e.g. 'RedshiftFromLine').
     """
 
-    all_targets = [ t_sets.z_target, t_sets.cd_target, t_sets.ld_target ]
+    all_targets = [ t_sets.z_target, t_sets.cd_target, t_sets.ld_target, t_sets.tl_target, t_sets.ta_target ]
 
     # Make sure only one target is specified (>= 0.0)
     if( sum(tarz >= 0.0 for tarz in all_targets) != 1 ): 
@@ -250,6 +293,20 @@ def RedshiftFromTarget(t_sets):
         return InvertLumDist( t_ld/pars.HubbleDistance() )
 
 
+    # Look-back Time
+    if( t_sets.tl_target >= 0.0 ):
+        t_tl = TimeToS(t_sets, t_sets.tl_target)
+        if( t_sets.verbose ): print " - Look-back Time, %.2e [s]" % (t_tl)
+        return InvertLookTime( t_tl/pars.HubbleTime() )
+
+
+    # Universe Age Time
+    if( t_sets.ta_target >= 0.0 ):
+        t_ta = TimeToS(t_sets, t_sets.ta_target)
+        if( t_sets.verbose ): print " - Age of the Universe, %.2e [s]" % (t_ta)
+        return InvertAgeTime( t_ta/pars.HubbleTime() )
+
+
     raise RuntimeError('Must specify a single target parameter.')
 
     
@@ -265,24 +322,35 @@ def InitArgParse():
     parser.add_argument('-z',         type=float, default=sets.z_target,       help='Target redshift z'                                        )
     parser.add_argument('-cd','-dc',  type=float, default=sets.cd_target,      help='Target coming distance D_C'                               )
     parser.add_argument('-ld','-dl',  type=float, default=sets.ld_target,      help='Target luminosity distance D_C'                           )
+    parser.add_argument('-tl','-lt',  type=float, default=sets.tl_target,      help='Target look-back time T_L'                                )
+    parser.add_argument('-ta','-at',  type=float, default=sets.ta_target,      help='Target universe age T_A'                                  )
 
-    parser.add_argument('--print', dest='prt',  action='store_true', default=False, help="Print defaul cosmological parameters"                  )
+    parser.add_argument('--print', dest='prt',  action='store_true', default=sets.print_flag, help="Print defaul cosmological parameters"                  )
     #parser.add_argument('--plot',               action='store_true', default=False, help="Plot cosmological parameters"                          )
-    parser.add_argument('--mpc',                action='store_true', default=False, help="Use provided distance ('-cd' or '-ld') in megaparsecs" )
-    parser.add_argument('--ly',                 action='store_true', default=False, help="Use provided distance ('-cd' or '-ld') in lightyears"  )
-    parser.add_argument('--pc',                 action='store_true', default=False, help="Use provided distance ('-cd' or '-ld') in parsecs"     )
+
+    parser.add_argument('--pc',                 action='store_true', default=sets.use_pc , help="Use provided distance ('-cd' or '-ld') in parsecs"     )
+    parser.add_argument('--mpc',                action='store_true', default=sets.use_mpc, help="Use provided distance ('-cd' or '-ld') in megaparsecs" )
+    parser.add_argument('--ly',                 action='store_true', default=sets.use_ly , help="Use provided distance ('-cd' or '-ld') in lightyears"  )
+
+    parser.add_argument('--yr',                 action='store_true', default=sets.use_yr , help="Use provided time ('-lt' or '-at') in years"           )
+    parser.add_argument('--myr',                action='store_true', default=sets.use_myr, help="Use provided time ('-lt' or '-at') in megayears"       )
+
 
     args            = parser.parse_args()
     sets.print_flag = args.prt
     #sets.plot_flag  = args.plot
+
     sets.z_target   = float(args.z)
     sets.cd_target  = float(args.cd)
     sets.ld_target  = float(args.ld)
+    sets.tl_target  = float(args.tl)
+    sets.ta_target  = float(args.ta)
+
     sets.use_pc     = args.pc
     sets.use_mpc    = args.mpc
     sets.use_ly     = args.ly
- 
-
+    sets.use_yr     = args.yr
+    sets.use_myr    = args.myr
 
 
 def main():
