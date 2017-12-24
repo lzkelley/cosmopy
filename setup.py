4fe21a4c080534fcdfc387a4332c75dfd6d4e103
lzkelley <lkelley@cfa.harvard.edu>
2017-12-23 19:29:51 -0800

    MAINT: add boilderplate

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+requirements = []
+
+setup(
+    name="cosmocalc",
+    version=2.0,
+    author="Luke Zoltan Kelley",
+    author_email="lkelley@cfa.harvard.edu",
+    description=("General, commonly used functions for other projects."),
+    license="MIT",
+    keywords="",
+    url="https://bitbucket.org/lzkelley/zcode/",
+    packages=['cosmocalc'],
+    include_package_data=True,
+    install_requires=requirements,
+    long_description=readme,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
